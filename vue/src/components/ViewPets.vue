d9667846aafb9981a76449b9f59496237d5192f9
Carlin Choi <carlinchoi@gmail.com>
2023-04-10 15:54:06 -0700

    Working on v-for loop to display pets

---
+++ vue/src/components/ViewPets.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <table>
+          <thead>
+              <tr>
+                  <th>Pet</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="petInfo in this.$store.state.petInfo" :key="petInfo.PetId">
+                  <td>{{ petInfo.breed }}</td>      
+              </tr>
+          </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
