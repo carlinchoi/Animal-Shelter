b206486cf4648bb470875164d12daba83020a494
Sarayuth Techaun <techaoun@gmail.com>
2023-04-10 11:29:17 -0500

    PetController

---
+++ java/src/main/java/com/techelevator/controller/PetController.java
@@ -0,0 +1,39 @@
+package com.techelevator.controller;
+
+import com.techelevator.model.Pet;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path="/pet")
+//TODO preAuthorize??
+public class PetController {
+    private Pet pet;
+
+    public PetController(Pet pet) {
+        this.pet = pet;
+    }
+    @RequestMapping(path="/all",method = RequestMethod.GET)
+    public List<Pet> listAllPet(){
+        //TODO list all pet from petDao.findAll()
+        List<Pet> allPet = petDao.findAll();
+        //TODO return List
+        return allPet;
+        
+    }
+    @RequestMapping(path="/{id}",method = RequestMethod.GET)
+    public Pet listPet(@PathVariable("id") int petId){
+        //TODO  get pet from petDao.findById()
+        Pet pet = petDao.findById(petId);
+        //TODO return List
+        return pet;
+    }
+
+
+}
