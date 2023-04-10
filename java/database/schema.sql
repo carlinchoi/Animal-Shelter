29f50a56e89c6c1dff7b498a5b29ce354e065289
Jenna Gigliotti <jenna3gigliotti@gmail.com>
2023-04-10 09:43:07 -0700

    Database SQL!

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,13 +1,82 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS administrators CASCADE;
+DROP TABLE IF EXISTS volunteers CASCADE;
+DROP TABLE IF EXISTS volunteer_application CASCADE;
+DROP TABLE IF EXISTS pets CASCADE;
+DROP TABLE IF EXISTS adoptions;
+
 
 CREATE TABLE users (
 	user_id SERIAL,
-	username varchar(50) NOT NULL UNIQUE,
-	password_hash varchar(200) NOT NULL,
-	role varchar(50) NOT NULL,
+	username VARCHAR(50) NOT NULL UNIQUE,
+	password_hash VARCHAR(200) NOT NULL,
+	role VARCHAR(50) NOT NULL,
+
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+CREATE TABLE volunteers (
+	volunteer_id SERIAL,
+	user_id INT NOT NULL,
+	volunteer_name VARCHAR(32) NOT NULL,
+	volunteer_role VARCHAR (64) NOT NULL,
+
+	CONSTRAINT PK_volunteer_id PRIMARY KEY (volunteer_id),
+	CONSTRAINT FK_users FOREIGN KEY (user_id) REFERENCES users(user_id)lkh
+
+);
+
+CREATE TABLE administrators (
+	administrator_id SERIAL,
+	user_id INT NOT NULL,
+	role VARCHAR(50) NOT NULL,
+
+	CONSTRAINT PK_administrator_id PRIMARY KEY (administrator_id),
+	CONSTRAINT FK_users FOREIGN KEY (user_id) REFERENCES users(user_id)
+
+);
+
+CREATE TABLE volunteer_application (
+	application_id SERIAL,
+	user_id INT NOT NULL,
+	application_status VARCHAR NOT NULL,
+
+	CONSTRAINT PK_application_id PRIMARY KEY (application_id),
+	CONSTRAINT FK_users FOREIGN KEY (user_id) REFERENCES users(user_id)
+
+);
+
+
+CREATE TABLE pets (
+	pet_id SERIAL,
+	pet_name varchar(50) NOT NULL UNIQUE,
+	pet_photo varChar (50),
+	is_adopted boolean NOT NULL,
+	species varChar(50) NOT NULL,
+	gender varChar (32) NOT NULL,
+	breed varChar (32) NOT NULL,
+	age_years int NOT NULL,
+	description varChar (100) NOT NULL,
+
+	CONSTRAINT PK_pet_id PRIMARY KEY (pet_id)
+
+);
+
+CREATE TABLE adoptions (
+	adoption_id SERIAL,
+	pet_id INT NOT NULL,
+	adoption_date Date,
+	parent_name VARCHAR(32) NOT NULL,
+	parent_email VARCHAR(32) NOT NULL,
+
+	CONSTRAINT adoption_id PRIMARY KEY (adoption_id),
+	CONSTRAINT FK_pets FOREIGN KEY (pet_id) REFERENCES pets(pet_id)
+);
+
+INSERT INTO pets (pet_name, pet_photo, is_adopted, species, gender, breed, age, description)
+	VALUES ('Cooper', 'doggie.com', 'True', 'Dog', 'male', 'Bernese Mountain Dog', '2', 'Black/White/Brown/fluffy and sweet');
+
 
 COMMIT TRANSACTION;
