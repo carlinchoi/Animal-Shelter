1ff73d4a52910e8e04265134dd8ed875fca80823
Jenna Gigliotti <jenna3gigliotti@gmail.com>
2023-04-12 11:19:46 -0700

    Small changes in footer

--- vue/src/components/TheFooter.vue
+++ vue/src/components/TheFooter.vue
@@ -21,13 +21,13 @@ export default {
   background-color: #034f3c;
   width: 100%;
   border: #00514f solid;
-  margin-bottom: 3px;
+  margin-bottom: 1px;
   position: fixed;
   bottom:0;
   left: 0;
   background-color: rgb(195, 226, 199);
   backdrop-filter: blur(10px);
-  height: 50px;
+  height: 100px;
   box-shadow: 0px 0px 5px;
 }
 
