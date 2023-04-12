be446bfdfae28fc72a9e238e0830eed3bca440fe
Carlin Choi <carlinchoi@gmail.com>
2023-04-12 10:12:15 -0700

    change Password

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -6,6 +6,7 @@ import Logout from '../views/Logout.vue'
 import Register from '../views/Register.vue'
 import store from '../store/index'
 import VolunteerRegister from '../views/VolunteerRegister.vue'
+import ChangePassword from '../views/ChangePassword.vue'
 
 Vue.use(Router)
 
@@ -58,6 +59,14 @@ const router = new Router({
       path: "/volunteer-register",
       name: "volunteer-register",
       component: VolunteerRegister,
+      meta: {
+        requiresAuth: false
+      }
+    },
+    {
+      path:"/change-password",
+      name:'change-password',
+      component: ChangePassword,
       meta: {
         requiresAuth: false
       }
