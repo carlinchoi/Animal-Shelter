b7af044e8c8feba5f002c9871e1e5dde240db8bd
Jenna Gigliotti <jenna3gigliotti@gmail.com>
2023-04-13 11:14:28 -0700

    Footer updates!

--- vue/src/components/TheFooter.vue
+++ vue/src/components/TheFooter.vue
@@ -1,8 +1,11 @@
 <template>
      <footer class="shelter-footer"> 
-             &copy; 2023. All rights reserved   
-            
+          <p class="copy"> &copy; 2023 Rockville Animal Shelter. All rights reserved.</p>   
+             <ion-icon name="logo-instagram"></ion-icon>
+             <ion-icon name="logo-facebook"></ion-icon>
+             <ion-icon name="logo-youtube"></ion-icon>
      </footer>
+   
 </template>
 
 
@@ -14,26 +17,39 @@ export default {
 
 <style>
 .shelter-footer  {
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 3px 10px;
-  background-color: #034f3c;
+  text-align: center;
+  padding: 7px 15px;
   font-family: Montserrat, sans-serif;
-  font-size: 18px;
-  padding-left: 100px;
+  font-size: 20px;
   color: white;
   width: 100%;
-  border: #00514f solid;
-  margin-bottom: 1px;
+  margin-bottom: 0px;
   position: fixed;
   bottom:0;
   left: 0px;
-  background-color: darkslategray;
+  background-color: #5c5470;;
   backdrop-filter: blur(10px);
   height: 75px;
   box-shadow: 0px 0px 5px;
+}
+
+.copy {
+ padding-left: 100px;
+ text-align: left;
+
+}
+
+ion-icon {
+  position: relative;
+  bottom: 20px;
+  font-size: 38px;
+  margin-top: -100px;
+  padding: 0px 10px;
+  
+}
+
+ion-icon:hover {
+  background-color: #a37da3;
 }
 
 </style>
