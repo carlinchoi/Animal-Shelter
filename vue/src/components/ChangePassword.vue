40b93d8ef5409e4d63e94bd565a6a8a47bbf8e86
Hannah Loosli <hmloosli@gmail.com>
2023-04-18 14:36:38 -0600

    Logo changes

--- vue/src/components/ChangePassword.vue
+++ vue/src/components/ChangePassword.vue
@@ -72,13 +72,24 @@ export default {
 
 <style scoped>
 
+body {
+  background-image: url("../assets/background2.png");
+  background-color: rgb(230, 222, 240);
+  background-repeat: repeat;
+  background-size: contain;
+  min-height: 100%;
+  min-width: 100%;
+  margin: 0;
+  padding: 0;
+}
 form {
   display: flex;
+  flex-wrap: wrap;
   flex-direction: column;
-  justify-content: center; 
-  align-items: center; 
-  height: 80vh;
-  margin-top: 20vh;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin-top: 0;
   max-width: 400px;
   margin: auto;
   padding: 80px;
@@ -86,26 +97,56 @@ form {
   border-radius: 10px;
   background-color: rgba(255, 255, 255, 0.671);
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
-  }
+}
 .form-input-group {
-  margin-bottom: 1rem;
+  margin-bottom: 0.5rem;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  align-items: center;
+}
+.form-input-group .icon {
+  width: 30px;
+  align-self: flex-start;
+  margin-right: -10px;
 }
 label {
   margin-right: 0.5rem;
 }
 input {
-  padding: 0.5rem;
+  padding: .5rem;
   border: 2px solid #ccc;
+  padding: 10px;
+  border-radius: 10px;
+}
+.placeholder-animate::-webkit-input-placeholder {
+  transition: all 0.3s ease-out;
+  opacity: 0.5;
+  transform: translateY(0px);
+}
+.placeholder-animate:focus + .floating-placeholder,
+.placeholder-animate.valid + .floating-placeholder {
+  font-size: 16px;
+  top: -10px;
+  left: 10px;
+  color: rgb(197, 172, 228);
+}
+.placeholder-animate {
+  width: 100%;
+  border: none;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
   border-radius: 5px;
-  
+  font-size: 16px;
+}
+.placeholder-animate:not(:focus) {
+  border-bottom: 1px solid #62a18f;
 }
 button {
   min-width: 130px;
   height: 40px;
   color: #fff;
-  padding: 5px 10px;
+  padding: -5px 5px;
   font-weight: bold;
   cursor: pointer;
   transition: all 0.3s ease;
@@ -115,18 +156,16 @@ button {
   border-radius: 20px;
   border: 2px solid #4c6e5c;
   background: #62a18f;
+  margin-top: 0px;
 }
 button:hover {
   background: #fff;
   color: #fff;
 }
-.login-wrapper {
+.register-wrapper {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-}
-* {
-overflow:auto;
 }
 </style>
