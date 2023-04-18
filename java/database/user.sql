7057c0ad62fc1af3fd165280421a47c72ccf4dc3
Sarayuth Techaun <techaoun@gmail.com>
2023-04-18 10:03:22 -0500

    sql fix

--- java/database/user.sql
+++ java/database/user.sql
@@ -1,12 +1,12 @@
 -- ********************************************************************************
 -- This script creates the database users and grants them the necessary permissions
 -- ********************************************************************************
+--
+CREATE USER final_capstone_owner
+WITH PASSWORD 'finalcapstone';
 
---CREATE USER final_capstone_owner
---WITH PASSWORD 'finalcapstone';
-
---CREATE USER final_capstone_appuser
---WITH PASSWORD 'finalcapstone';
+CREATE USER final_capstone_appuser
+WITH PASSWORD 'finalcapstone';
 
 GRANT ALL
 ON ALL TABLES IN SCHEMA public
