ddffba24eb29587a438ef3943c2f2540bfe05cf1
Carlin Choi <carlinchoi@gmail.com>
2023-04-20 08:34:53 -0700

    Added more dummy data

--- java/database/InsertAdminVolDummy.sql
+++ java/database/InsertAdminVolDummy.sql
@@ -1,8 +1,46 @@
 INSERT INTO public.users(
 	 username, password_hash, role, email, first_name, last_name, phone)
-	VALUES ('Admin2', 'admin2', 'ROLE_ADMIN', 'admin2@gmail.com', 'admin2Name','admin2Lastname' , '999999');
-	
+	VALUES ('Jack', 'admin', 'ROLE_ADMIN', 'admin@gmail.com', 'Jack','Black' , '323-929-5833');
+
+
 	INSERT INTO public.users(
-	 username, password_hash, role, email, first_name, last_name, phone)
-	VALUES ('Vol2', 'vol2', 'ROLE_VOLUNTEER', 'vol2@gmail.com', 'vol2Name','vol2Lastname' , '77777777');
-	
+         username, password_hash, role, email, first_name, last_name, phone)
+        VALUES ('jason_smith', 'jasonpass', 'ROLE_VOLUNTEER', 'jason_smith@gmail.com', 'Jason', 'Smith', '212-555-1234');
+
+    INSERT INTO public.users(
+         username, password_hash, role, email, first_name, last_name, phone)
+        VALUES ('lisa_garcia', 'lisapass', 'ROLE_VOLUNTEER', 'lisa_garcia@gmail.com', 'Lisa', 'Garcia', '310-555-5678');
+
+    INSERT INTO public.users(
+         username, password_hash, role, email, first_name, last_name, phone)
+        VALUES ('andrew_miller', 'andrewpass', 'ROLE_VOLUNTEER', 'andrew_miller@gmail.com', 'Andrew', 'Miller', '312-555-9012');
+
+    INSERT INTO public.users(
+         username, password_hash, role, email, first_name, last_name, phone)
+        VALUES ('natalie_johnson', 'nataliepass', 'ROLE_VOLUNTEER', 'natalie_johnson@gmail.com', 'Natalie', 'Johnson', '415-555-3456');
+
+    INSERT INTO public.users(
+         username, password_hash, role, email, first_name, last_name, phone)
+        VALUES ('ryan_brown', 'ryanpass', 'ROLE_VOLUNTEER', 'ryan_brown@gmail.com', 'Ryan', 'Brown', '617-555-7890');
+
+INSERT INTO public.users(
+     username, password_hash, role, email, first_name, last_name, phone)
+    VALUES ('jenny_williams', 'jennypass', 'ROLE_PENDINGVOLUNTEER', 'jenny_williams@gmail.com', 'Jenny', 'Williams', '310-555-1234');
+
+INSERT INTO public.users(
+     username, password_hash, role, email, first_name, last_name, phone)
+    VALUES ('peter_nguyen', 'peterpass', 'ROLE_PENDINGVOLUNTEER', 'peter_nguyen@gmail.com', 'Peter', 'Nguyen', '415-555-5678');
+
+INSERT INTO public.users(
+     username, password_hash, role, email, first_name, last_name, phone)
+    VALUES ('maria_ramirez', 'mariapass', 'ROLE_PENDINGVOLUNTEER', 'maria_ramirez@gmail.com', 'Maria', 'Ramirez', '312-555-9012');
+
+INSERT INTO public.users(
+     username, password_hash, role, email, first_name, last_name, phone)
+    VALUES ('kevin_kim', 'kevinpass', 'ROLE_PENDINGVOLUNTEER', 'kevin_kim@gmail.com', 'Kevin', 'Kim', '617-555-3456');
+
+INSERT INTO public.users(
+     username, password_hash, role, email, first_name, last_name, phone)
+    VALUES ('lisa_jones', 'lisapass', 'ROLE_PENDINGVOLUNTEER', 'lisa_jones@gmail.com', 'Lisa', 'Jones', '212-555-6789');
+
+
