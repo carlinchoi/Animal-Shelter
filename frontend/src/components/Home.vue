c0d81d184af88c2b2031495d701bfb59b768802c
Carlin Choi <carlinchoi@gmail.com>
2023-04-25 02:22:31 -0700

    Styling on header, and text for homepage complete

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,142 @@
+<template>
+  <div id="app" style="min-height: 100vh;">
+    <q-layout view="lHh lpr lFf" container style="height: 500px" class="shadow-2 rounded-borders header">
+      <q-header class='text-black bg-white' elevated>
+        <q-toolbar>
+          <q-avatar>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTet9ewjLhjRL3uQUvKqGCeIdZqreI1Csmxow&usqp=CAU">
+          </q-avatar>
+
+          <q-toolbar-title>Rockville Animal Shelter</q-toolbar-title>
+
+
+          <q-tabs v-model="tab" class="q-tabs-centered">
+            <q-tab name="images" label="Home"></q-tab> |
+            <q-tab name="videos" label="Our Adopted Pets"></q-tab> |
+            <q-tab name="articles" label="Add New Pet Listing" class="q-tab-with-margin"></q-tab>
+          </q-tabs>
+
+
+          <q-btn color="primary" label="Account">
+            <q-menu>
+              <q-list dense style="min-width: 100px">
+                <q-item clickable v-close-popup>
+                  <q-item-section>Open...</q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup>
+                  <q-item-section>New</q-item-section>
+                </q-item>
+                <q-separator></q-separator>
+                <q-item clickable>
+                  <q-item-section>Preferences</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
+
+          <q-menu>
+            <q-avatar><img src='https://cdn-icons-png.flaticon.com/512/5087/5087579.png'></q-avatar>
+          <q-tab label="Account" class="q-tab-right">
+              <q-list>
+                <q-item clickable>
+                  <q-item-section>
+                    Logout
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-tab>
+          </q-menu>
+        </q-toolbar>
+      </q-header>
+
+      <div class="home-text-container">
+  <div class="home-title-container">
+    <h1 class='fixed-center' style ='font-weight:bold'>
+      Every Pet Deserves a Loving Home. <br>
+      <span style='color:orange'>Adopt </span>
+      <span>a Pet Today!</span>
+    </h1>
+    <p class="fixed-bottom">
+      Browse our available animals and learn more about the adoption process. Together, we can
+      <span style='font-weight: bold;'> rescue, rehabilitate, and rehome pets in need.</span>
+      Thank you for supporting our mission to bring joy to families through pet adoption!
+    </p>
+  </div>
+</div>
+
+
+    </q-layout>
+  </div>
+
+
+
+</template>
+
+
+<script>
+import { defineComponent, ref } from 'vue'
+name: "HeaderComponent"
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
