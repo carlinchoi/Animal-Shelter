81d44df23be1241a3d395dc719467acb33f8a739
Carlin Choi <carlinchoi@gmail.com>
2023-05-14 20:54:32 -0700

    Added explicit baseURL to PetService

--- java/database/create.sh
+++ java/database/create.sh
@@ -11,3 +11,4 @@ psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/fakeAnimals.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/InsertAdminVolDummy.sql"
+
