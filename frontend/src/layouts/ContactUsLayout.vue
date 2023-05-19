fd9df46cf7be523c4ee5e2b458dc703be3359720
Carlin Choi <carlinchoi@gmail.com>
2023-05-18 23:05:19 -0700

    Added header and footer to contactUs

---
+++ frontend/src/layouts/ContactUsLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout view="hHh lpr fFf">
+    <AppHeader />
+    <q-page-container @scroll="onScroll">
+      <ContactUs />
+    </q-page-container>
+    <AppFooter />
+  </q-layout>
+</template>
+
+<script>
+import ContactUs from "../components/ContactUs.vue";
+import AppFooter from "../components/AppFooter.vue";
+import AppHeader from "../components/AppHeader.vue";
+
+export default {
+  name: "FaqLayout",
+  components: {
+    ContactUs,
+    AppFooter,
+    AppHeader,
+  },
+};
+</script>
---
+++ frontend/src/pages/ContactUsPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <ContactUsLayout />
+</template>
+
+<script>
+import ContactUsLayout from "src/layouts/ContactUsLayout.vue";
+
+export default {
+  components: {
+    ContactUsLayout,
+  },
+};
+</script>
